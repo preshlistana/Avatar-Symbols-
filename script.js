c578333ede9a3802d90ebdb9811289a451ed096d
Presh <preshlistana@berkeley.edu>
2020-07-24 01:04:05 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(220);
+  // Water symbol 
+  noFill();
+  stroke(5);
+  circle(width/2, height/2, 100);
+  circle(225, 225, 25);
+  circle(215, 250, 25);
+  circle(250, 220, 25);
+
+}
