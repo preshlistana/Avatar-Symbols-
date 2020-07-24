17c8549c78c6a5a84d28003eec46ccbfb19ed924
Presh <preshlistana@berkeley.edu>
2020-07-24 01:06:51 +0000

    Added in function to draw fire element

--- script.js
+++ script.js
@@ -5,11 +5,20 @@ function setup() {
 function draw() {
   background(220);
   // Water symbol 
-  noFill();
-  stroke(5);
-  circle(width/2, height/2, 100);
-  circle(225, 225, 25);
-  circle(215, 250, 25);
-  circle(250, 220, 25);
+  // noFill();
+  // stroke(5);
+  // circle(width/2, height/2, 100);
+  // circle(225, 225, 25);
+  // circle(215, 250, 25);
+  // circle(250, 220, 25);
+   drawFireNation();
+}
 
+function drawFireNation() {
+  noFill();
+  strokeWeight(5);
+  stroke(color(161, 0, 0));
+
+  triangle(210, 270, 220, 270, 215, 200)
+  ellipse(250, 250, 60, 60);
 }
